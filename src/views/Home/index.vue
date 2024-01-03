dc63fb55f7961862c3f8844839f4bac847afb717
bingnuoer <2366858264@qq.com>
2024-01-03 20:05:23 +0800

    home-面板组件封装（props和插槽）

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,15 +3,27 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
-  <div class="container">
-    <HomeCategory />
-    <HomeBanner />
-  </div>
-  <HomeNew />
-  <HomeHot />
-  <homeProduct />
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
+    </div>
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
+
+    <!-- 面板部分 -->
+    <!-- 使用插槽-模板复用 -->
+    <!-- 使用标题和副标题 -->
+    <HomePanel title="新鲜好物" subTitle="新鲜好物 好多内容">
+        <!-- 使用插槽 -->
+        <div>我是新鲜好物的内容区域</div>
+    </HomePanel>
+    <HomePanel title="人气推荐" subTitle="人气推荐 好多内容">
+        <div>我是人气推荐的内容区域</div>
+    </HomePanel>
 </template>
