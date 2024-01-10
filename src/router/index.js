e20d2ce262caac21d1c1bd8d23784020deb423e9
bingnuoer <2366858264@qq.com>
2024-01-10 11:11:12 +0800

    二级分类-整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,19 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+// 配置路由-导入二级分类页
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     // 给一级网页配置路由路径
     {
+      // 相当于给这个网页配跳转路由地址-在配置path路由路径的网页点击跳转，就会跳转到component配置的页面
+
+      // 网页跳转路由地址
       path: '/',
+      // 对应的组件
       component: Layout,
       // 配置二级路由路径：一级网页下的二级路由
       // 并 在对应的一级网页页面配置二级路由出口
@@ -27,6 +33,13 @@ const router = createRouter({
           path:'category/:id',
           name:'category',
           component:Category
+        },
+        // 二级分类页
+        {
+          // 动态路由，参数id
+          path:'category/sub/:id',
+          name:'subcategory',
+          component:SubCategory
         }
       ]
     },
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -44,7 +44,8 @@ const { bannerList } = useBanner()
                 <h3>全部分类</h3>
                 <ul>
                     <li v-for="i in categoryData.children" :key="i.id">
-                        <RouterLink to="/">
+                        <!-- 这块实现点击跳转到 二级分类页路由 -->
+                        <RouterLink :to="`/category/sub/${i.id}`">
                             <img :src="i.picture" />
                             <p>{{ i.name }}</p>
                         </RouterLink>
