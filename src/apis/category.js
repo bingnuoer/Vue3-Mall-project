33e31f5a7215a65333725ade5fcaedd143160fc7
bingnuoer <2366858264@qq.com>
2024-01-10 11:41:22 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -10,3 +10,18 @@ export function getCategoryAPI(id) {
         }
     })
 }
+
+/**
+ * @description: 获取二级分类列表数据-面包屑导航
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+
+export const getCategoryFilterAPI = (id) => {
+    return request({
+      url:'/category/sub/filter',
+      params:{
+        id
+      }
+    })
+  }
