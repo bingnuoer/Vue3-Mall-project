fcf43c0541fc33842447bf0a18a761b672f06f41
bingnuoer <2366858264@qq.com>
2024-01-13 16:44:21 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,7 +5,8 @@ import { ref } from 'vue'
 // 1.准备表单对象
 const form = ref({
   account: '',
-  password: ''
+  password: '',
+  agree: '' //是否同意协议
 })
 
 // 2.表单校验规则
@@ -16,7 +17,23 @@ const rules = {
   password: [
     { required: true, message: '密码不能为空', blur: true },
     { min: 6, max: 14, message: '密码长度为6-14位', blur: true }
+  ],
+  //是否同意协议-自定义校验规则
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        console.log(value); //true、false
+        // 自定义校验逻辑
+        // 勾选就通过，不勾选就不通过
+        if (value) {
+          callback()
+        } else {
+          callback(new Error('请勾选协议'))
+        }
+      }
+    }
   ]
+
 }
 
 </script>
@@ -49,13 +66,13 @@ const rules = {
               <!-- 绑定表单域校验字段名 -->
               <el-form-item prop="account" label="账户">
                 <!-- 表单数据双向绑定 -->
-                <el-input v-model="form.account"/>
+                <el-input v-model="form.account" />
               </el-form-item>
               <el-form-item prop="password" label="密码">
-                <el-input v-model="form.password"/>
+                <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
