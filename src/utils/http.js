7b3216e0da47059bf53b3b37cea22109f153a8a9
bingnuoer <2366858264@qq.com>
2024-02-26 15:25:12 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 // 弹框
 import { ElMessage } from 'element-plus'
+
+// token请求头
+import { useUserStore } from '@/stores/user.js'
+
 // 解决样式覆盖
 import 'element-plus/theme-chalk/el-message.css'
 
@@ -12,6 +16,16 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  // 接口携带请求头
+  // 1.从pinia获取token数据
+  const userStore = useUserStore()
+  // 2.按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if (token) {
+    // 把token配置在请求头中 配置请求头固定写法
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
   return config
 }, e => Promise.reject(e))
 
